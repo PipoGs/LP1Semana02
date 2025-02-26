fce62640ed4f9139662f10f56700addb355c12f9
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-02-26 14:37:53 +0000

    Added Code for SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks.Dataflow;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere número inteiro: ");
+            string input = Console.ReadLine();
+            sbyte n = sbyte.Parse(input);
+            
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+
+
+        }
+    }
+}
