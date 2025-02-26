ae8bb28e4f85d5c00960dbac4c14b03e81230b65
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-02-26 19:54:03 +0000

    Added Exit Function to break cycle

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 
-namespace ChatGPTOne
+namespace ChatGPTZero
 {
     public class Program
-    {
+    {   
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            while (true)
+            {
+                // Variáveis auxiliares
+                string question, response;
+
+                // Colocar questão ao utilizador
+                Console.Write("Place your question? ");
+                question = Console.ReadLine();
+
+                // Verificar se se trata de uma questão conhecida e determinar uma
+                // resposta adequada
+                switch (question)
+                {
+                    case "How are you?":
+                        response = "I'm fine, thank you";
+                        break;
+                    case "What's your name?":
+                        response = "Gran'pa";
+                        break;
+                    case "What's your mission?":
+                        response = "Destroy mankind!";
+                        break;
+                    case "Are you more intelligent than me?":
+                        response = "Obviously!";
+                        break;
+                    case "EXIT":
+                        return;
+                    default:
+                        response = "You got me, I'm not THAT smart!";
+                        break;
+                        
+                }
+                // Responder adequadamente
+                Console.WriteLine(response);
+            }
         }
     }
 }
