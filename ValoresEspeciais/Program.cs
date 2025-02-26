3ee1a2d560154221f51b88c4212f15443cf82d38
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-02-26 18:01:32 +0000

    Added First part of Valores Especiais

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Valores máximos e mínimos dos tipos numéricos
+        Console.WriteLine("Valores Máximos e Mínimos:");
+
+        // int
+        Console.WriteLine($"Máximo valor int: {int.MaxValue}");
+        Console.WriteLine($"Mínimo valor int: {int.MinValue}");
+
+        // long
+        Console.WriteLine($"Máximo valor long: {long.MaxValue}");
+        Console.WriteLine($"Mínimo valor long: {long.MinValue}");
+
+        // float
+        Console.WriteLine($"Máximo valor float: {float.MaxValue}");
+        Console.WriteLine($"Mínimo valor float: {float.MinValue}");
+
+        // double
+        Console.WriteLine($"Máximo valor double: {double.MaxValue}");
+        Console.WriteLine($"Mínimo valor double: {double.MinValue}");
+
+        // decimal
+        Console.WriteLine($"Máximo valor decimal: {decimal.MaxValue}");
+        Console.WriteLine($"Mínimo valor decimal: {decimal.MinValue}");
+        }
+    }
+}
